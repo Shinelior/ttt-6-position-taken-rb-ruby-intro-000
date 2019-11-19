45724b368424b8d20d28ea1c502cd9dedd3ddd9d
lior Shine <shinelior@gmail.com>
2019-11-19 06:26:16 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
-  if board[index]!="O" || board[index]!="X"
-     return false
-  else
+  if board[index]=="O" || board[index]=="X"
      return true
+  else
+     return false
   end
 end  
  
